0d25edca5b0ccf1f3ecff9de000be6a2b51efe54
AnkitPaswan <ankitpaswan192@gmail.com>
2024-03-09 19:40:13 +0530

    print table

--- javascript/loops/for-in-loop.js
+++ javascript/loops/for-in-loop.js
@@ -1,13 +1,10 @@
-
 // 08/03/24
-
 
 // for in:--Array
 // let array = [10,20,30,40];
 // for (const index in array) {
-//         console.log(array[index]) ;2
+//         console.log(array[index]) ;
 // }
-
 
 // for in:--String
 // let string = "DUCAT"
@@ -15,19 +12,19 @@
 //         console.log(string[index]) ;
 // }
 
-
 // for in:--Object
 let object = {
-    fname: "Ankit",
-    lname: "Paswan"
+  fname: "Ankit",
+  lname: "Paswan",
+};
+
+
+for (const key in object) {
+        console.log(key, object[key]) ;
 }
+
 
 // console.log(object.fname);
 
 // let len = object.lname.length;
 // console.log(len);
-
-// for (const key in object) {
-//         console.log(key, object[key]) ;
-// }
-
--- practice/print-table-using-for-in-loop.js
+++ practice/print-table-using-for-in-loop.js
@@ -1,3 +1,32 @@
 
 
-console.log("hello");
+
+// let number = prompt("enter a number to print the table:-");
+
+//using for loop
+// for (let i = 1; i <= 10; i++) {
+//     document.write(number + " x " + i + " = " + number * i);
+//     document.write("<br>");
+// }
+
+//using while loop
+// let i =1;
+// while (i < 10) { 
+//     console.log(number + " x " + i + " = " + number * i);
+//     i++;
+// }
+
+
+
+//print the table using for-in loop
+let number = prompt("enter a number to print the table:-");
+
+let table = [];
+for (let i = 1; i <= 10; i++) {
+  table.push(number * i);
+}
+
+for (let i in table) {
+  document.write(number + " x " + (parseInt(i) + 1) + " = " + table[i]);
+  document.write("<br>");
+}
