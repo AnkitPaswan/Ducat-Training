caa7adbcf94b3856a7b41ff278109e86169730a4
AnkitPaswan <ankitpaswan192@gmail.com>
2024-04-23 09:55:29 +0530

    tag ,query,class,queryselectorAll

--- javascript/DOM/script.js
+++ javascript/DOM/script.js
@@ -1,37 +1,84 @@
-//selectors
-let heading = document.getElementById("heading");
-console.log(heading);
+// //selectors
+// let heading = document.getElementById("heading");
+// console.log(heading);
 
-//get data
-console.log(heading.innerText);
-console.log(heading.innerHTML);
+// //get data
+// console.log(heading.innerText);
+// console.log(heading.innerHTML);
 
-//set data
+// //set data
 
-heading.innerText = "Hello World <button>Button<button";
-heading.innerHTML = "Hello World <button>Button<button>";
+// heading.innerText = "Hello World <button>Button<button";
+// heading.innerHTML = "Hello World <button>Button<button>";
 
-//id
-console.log(heading.id);
-heading.id = "head";
+// //id
+// console.log(heading.id);
+// heading.id = "head";
+
+// //class
+// console.log(heading.className);
+
+// heading.className = "red";
+// console.log(heading.classList);
+
+// heading.classList.add("red");
+// heading.classList.remove("testing");
+// heading.classList.toggle("red");
+
+// //style
+// console.log(heading.style);
+// heading.style.backgroundColor = "red";
+// heading.style.color = "white";
+// heading.style.padding = "10px";
+
+// //remove
+// heading.remove();
+
+// ------------------------------------------------------
 
 //class
-console.log(heading.className);
+// let paragraphs = document.getElementsByClassName("red");
+// console.log(paragraphs);
 
-heading.className = "red";
-console.log(heading.classList);
+// paragraphs[0].innerHTML = "Testing";
+// paragraphs[0].style.backgroundColor = "black";
 
-heading.classList.add("red");
-heading.classList.remove("testing");
-heading.classList.toggle("red");
+// for (const element of paragraphs) {
+//   element.innerHTML = "Testing";
+//   element.style.backgroundColor = "black";
+// }
 
-//style
-console.log(heading.style);
-heading.style.backgroundColor = "red";
-heading.style.color = "white";
-heading.style.padding = "10px";
+//tags/element selector
 
-//remove
-heading.remove();
+// let paragraphs = document.getElementsByTagName("p");
+// console.log(paragraphs);
 
-//class
+// paragraphs[0].innerHTML = "Testing";
+// paragraphs[0].style.backgroundColor = "black";
+
+// for (const element of paragraphs) {
+//   element.innerHTML = "Testing";
+//   element.style.backgroundColor = "black";
+// }
+
+//querySelector
+// let heading = document.querySelector("#heading"); //id
+// let paragraphs = document.querySelector(".red"); //class
+// let paragraphs = document.querySelector("p"); //tag/element
+// console.log(paragraphs);
+
+//querySelectorAll
+// let heading = document.querySelectorAll("#heading"); //id
+// let paragraphs = document.querySelectorAll(".red"); //class
+// let paragraphs = document.querySelectorAll("p"); //tag/element
+// console.log(paragraphs);
+
+//how to create element via dom
+let div = document.createElement("div");
+console.log(div);
+heading.innerHTML = "Div cerated by dom";
+div.className = "red";
+
+let root = document.getElementById("root");
+root.append(div);
+root.prepend(div);
