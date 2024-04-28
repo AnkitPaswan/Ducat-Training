8bcd95e774a07467b8856e022fef321b3e93de5d
AnkitPaswan <ankitpaswan192@gmail.com>
2024-04-28 12:43:21 +0530

    api call done

--- tasks/24-04-24/script.js
+++ tasks/24-04-24/script.js
@@ -8,45 +8,70 @@
 </div> */
 }
 
-//create parentdiv
-const parentDiv = document.createElement("div");
-parentDiv.className = "mt-5";
+// //create parentdiv
+// const parentDiv = document.createElement("div");
+// parentDiv.className = "mt-5";
 
-//append to root
-root.append(parentDiv);
+// //append to root
+// root.append(parentDiv);
 
-//create card div
-const card = document.createElement("div");
-card.className = "card";
-card.style = "width: 18rem";
+// //create card div
+// const card = document.createElement("div");
+// card.className = "card mx-auto";
+// card.style = "width: 15rem";
 
-//appent to parentdiv
-parentDiv.append(card);
+// //appent to parentdiv
+// parentDiv.append(card);
 
-//image div
-const image = document.createElement("img");
-image.className = "card-img-top";
-image.src = "https://placehold.co/600x400";
-image.alt = "Card image cap";
+// //image div
+// const image = document.createElement("img");
+// image.className = "card-img-top";
+// image.src = "https://placehold.co/600x400";
+// image.alt = "Card image cap";
 
-//card body
+// //card body
 
-const cardBody = document.createElement("div");
-cardBody.className = "card-body";
-//append to card div
-card.append(image);
-card.append(cardBody);
+// const cardBody = document.createElement("div");
+// cardBody.className = "card-body";
+// //append to card div
+// card.append(image);
+// card.append(cardBody);
 
-//h5
-const h5 = document.createElement("h5");
-h5.className = "card-title text-center";
-h5.innerText = "15°C";
+// //h5
+// const h5 = document.createElement("h5");
+// h5.className = "card-title text-center";
+// h5.innerText = "15°C";
 
-//p
-const p = document.createElement("p");
-p.className = "card-text text-center";
-p.innerText = "this is weather info";
+// //p
+// const p = document.createElement("p");
+// p.className = "card-text text-center";
+// p.innerText = "this is weather info";
 
-//append to card body
-cardBody.append(h5);
-cardBody.append(p);
+// //append to card body
+// cardBody.append(h5);
+// cardBody.append(p);
+
+const loc = document.getElementById("location");
+const search = document.getElementById("search");
+const weather = document.getElementById("weather");
+const API_KEY = "2d0b505bc82420738afcbd80308892e8";
+
+search.addEventListener("click", function () {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${loc.value}&appid=${API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const currentWeather = data.weather[0].description;
+      const currentTemp = Math.round(data.main.temp - 273.15);
+
+      weather.innerHTML = `
+                <p>Current weather: ${currentWeather}</p>
+                <p>Current temperature: ${currentTemp}°C</p>
+            `;
+      console.log(currentWeather);
+    })
+    .catch((error) => {
+      weather.innerHTML = `<p>Error: ${error}</p>`;
+    });
+});
