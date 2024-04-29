31f3897308e33c7a0afb2d2a82466a55c2d304eb
AnkitPaswan <ankitpaswan192@gmail.com>
2024-04-29 11:08:44 +0530

    weather project done

--- tasks/24-04-24/script.js
+++ tasks/24-04-24/script.js
@@ -1,77 +1,38 @@
-{
-  /* <div class="card" style="width: 18rem">
-<img src="https://placehold.co/600x400" class="card-img-top" alt="...">
-<div class="card-body">
-  <h5 class="card-title">Card title</h5>
-  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-</div>
-</div> */
-}
-
-// //create parentdiv
-// const parentDiv = document.createElement("div");
-// parentDiv.className = "mt-5";
-
-// //append to root
-// root.append(parentDiv);
-
-// //create card div
-// const card = document.createElement("div");
-// card.className = "card mx-auto";
-// card.style = "width: 15rem";
-
-// //appent to parentdiv
-// parentDiv.append(card);
-
-// //image div
-// const image = document.createElement("img");
-// image.className = "card-img-top";
-// image.src = "https://placehold.co/600x400";
-// image.alt = "Card image cap";
-
-// //card body
-
-// const cardBody = document.createElement("div");
-// cardBody.className = "card-body";
-// //append to card div
-// card.append(image);
-// card.append(cardBody);
-
-// //h5
-// const h5 = document.createElement("h5");
-// h5.className = "card-title text-center";
-// h5.innerText = "15°C";
-
-// //p
-// const p = document.createElement("p");
-// p.className = "card-text text-center";
-// p.innerText = "this is weather info";
-
-// //append to card body
-// cardBody.append(h5);
-// cardBody.append(p);
+// const API_KEY = "2d0b505bc82420738afcbd80308892e8";
+// const weather = document.getElementById("Weather");
+const apiKey = "2d0b505bc82420738afcbd80308892e8";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 
 const loc = document.getElementById("location");
 const search = document.getElementById("search");
-const weather = document.getElementById("weather");
-const API_KEY = "2d0b505bc82420738afcbd80308892e8";
+const locName = document.getElementById("name");
+const temp = document.getElementById("temp");
+const desc = document.getElementById("desc");
+const icons = document.getElementById("icon");
 
-search.addEventListener("click", function () {
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${loc.value}&appid=${API_KEY}`
-  )
+search.addEventListener("click", () => {
+  const location = loc.value;
+  if (location) {
+    fetchWeather(location);
+  }
+});
+
+function fetchWeather(location) {
+  const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const currentWeather = data.weather[0].description;
-      const currentTemp = Math.round(data.main.temp - 273.15);
+      console.log(data);
+      const icon = data.weather[0].icon;
+      const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+      icons.innerHTML = `<img src="${iconUrl}" />`;
 
-      weather.innerHTML = `
-                <p>Current weather: ${currentWeather}</p>
-                <p>Current temperature: ${currentTemp}°C</p>
-            `;
-      console.log(currentWeather);
+      locName.innerText = data.name;
+      temp.innerText = `${Math.round(data.main.temp)}°C`;
+      desc.innerText = data.weather[0].description;
     })
     .catch((error) => {
-      weather.innerHTML = `<p>Error: ${error}</p>`;
+      console.error("Error fetching weather data:", error);
     });
-});
+}
